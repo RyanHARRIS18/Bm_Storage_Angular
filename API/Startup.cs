ae3fc9719512835615f641049927f2b7e88cc9e7
Ryan Harris <ryanpaulharris@byui.edu>
2020-10-03 15:43:48 -0600

    section3

--- API/Startup.cs
+++ API/Startup.cs
@@ -53,6 +53,9 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod()
+            .WithOrigins("https://localhost:4200"));
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
