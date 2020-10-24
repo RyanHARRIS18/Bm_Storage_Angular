41d2b99738ee3a911cb077a4711078b2a1f34393
Ryan Harris <ryanpaulharris@byui.edu>
2020-10-23 18:08:25 -0600

    End of Section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -23,9 +23,6 @@ export class NavComponent implements OnInit {
   login(){
     this.accountService.login(this.model).subscribe(response => {
        this.router.navigateByUrl('/tenants');
-      }, error => {
-        console.log(error);
-        this.toastr.error(error.error);
       })
   }
 
